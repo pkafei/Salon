2d6c63d491ba2a5a077131a75f9a4f6876983f68
Portia Burton <plburton@gmail.com>
2016-02-24 21:42:39 -0500

    Updated urls

--- salon_app/salon_app/urls.py
+++ salon_app/salon_app/urls.py
@@ -3,10 +3,9 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    # Examples:
     url(r'^$', views.Landing.as_view(), name='landing'),
     url(r'^contact/', views.Contact.as_view(), name='contact'),
-    url(r'^services/', views.Contact.as_view(), name='service'),
+    url(r'^services/', views.Contact.as_view(), name='services'),
     url(r'blog/', include('blog.urls')),
     #url(r'^$', 'salon_app.views.home', name='home'),
     #url(r'^blog/', include('blog.urls')),
