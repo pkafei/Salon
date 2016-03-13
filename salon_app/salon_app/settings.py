cea92c12212e397d80d5e3f4ba3e26bf57a39730
Portia Burton <plburton@gmail.com>
2016-03-13 16:40:09 -0400

    Added app site_builder

--- salon_app/salon_app/settings.py
+++ salon_app/salon_app/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'blog',
     'salon_app',
+    'site_builder',
     'wagtail.wagtailforms',
     'wagtail.wagtailredirects',
     'wagtail.wagtailembeds',
